713f44c2ac69b942bf606e47d1bfebbc32e9cf85
Shamil Sazho <shamilsazho@192.168.1.30.non-exists.ptr.local>
2024-11-20 14:04:04 +0700

    config changed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.cocktailapp.app',
   appName: 'Cocktail Recipes',
   webDir: 'www',
-  server: {
-    url: 'http://localhost:8100',
-    androidScheme: 'https',
-    cleartext: true
-  }
 };
 
 export default config;
